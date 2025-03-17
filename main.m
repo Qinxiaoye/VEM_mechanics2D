a55ae58e6e4006e15ce7ecbd13e5c9794edf140a
Bingbing Xu <49779828+Qinxiaoye@users.noreply.github.com>
2025-03-17 13:22:21 +0100

    all

---
+++ main.m
@@ -0,0 +1,53 @@
+% Copyright:
+% Bingbing Xu, Peter Wriggers
+% Contact:
+% xubingbingdut@foxmail.com, bingbing.xu@ikm.uni-hannover.de
+% Institute of Continuum Mechanics, Leibniz Universität Hannover
+
+clear;
+load("cook.mat"); % you can generate mesh by your self
+
+k = 1; % first order VEM for elasticity
+
+E = 200000; nu = 0.3; % material parameters
+mat.E = E; mat.nu = nu;
+
+sumNode = size(node,1);
+
+p = 100; % press
+
+% boundary condition
+nodeL = find(node(:,1)<0.001);
+sL = size(nodeL,1);
+nodeR  = find(node(:,1)>48-0.001);
+% find face by node
+pface = findFace(node,elem,nodeR);
+press = [pface,ones(length(pface),1)*p];
+fixNode = [nodeL,ones(sL,1),zeros(sL,1);nodeL,2*ones(sL,1),zeros(sL,1)];
+fixMes = [fixNode(:,1)+(fixNode(:,2)-1)*sumNode,fixNode(:,3)];
+
+nodeForce = getForce(node,elem,press,'y');
+
+% global stiffness matrix
+GK = globalK(node,elem,mat);
+
+[GK,F] = boundaryCondition(GK,nodeForce,fixMes(:,1),fixMes(:,2),1);
+
+uh = GK\F;
+
+uh = full(uh);
+
+ux = uh(1:sumNode);
+uy = uh(sumNode+1:end);
+
+
+% stress
+[stress,mises] = calculateStress(node,elem,uh,mat);
+
+figure;
+showsolution(node+[ux,uy],elem,mises);
+figure;
+showsolution(node+[ux,uy],elem,uy);
+
+
+
---
+++ showsolution.m
@@ -0,0 +1,30 @@
+function showsolution(node,elem,u,varargin)
+%Showsolution displays the solution corresponding to a mesh given by [node,elem] in 2-D.
+%
+% Copyright (C) Terence Yu.
+
+data = node;
+if ~iscell(elem)
+    patch('Faces', elem,...
+        'Vertices', data,...
+        'FaceColor', 'interp',...
+        'CData', u);
+else
+    max_n_vertices = max(cellfun(@length, elem));
+    padding_func = @(vertex_ind) [vertex_ind,...
+        NaN(1,max_n_vertices-length(vertex_ind))];  % function to pad the vacancies
+    tpad = cellfun(padding_func, elem, 'UniformOutput', false);
+    tpad = vertcat(tpad{:});
+    patch('Faces', tpad,...
+        'Vertices', data,'EdgeColor','k',...
+        'FaceColor', 'interp',...
+        'CData', u);
+end
+axis equal; axis off;
+sh = 0.0;
+xlim([min(node(:,1)) - sh, max(node(:,1)) + sh])
+ylim([min(node(:,2)) - sh, max(node(:,2)) + sh])
+
+xlabel('x'); ylabel('y'); 
+
+
